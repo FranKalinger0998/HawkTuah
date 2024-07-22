a2b3ab725068752bc73224c8bf3674963f9c61a9
FranKalinger0998 <53475653+FranKalinger0998@users.noreply.github.com>
2024-07-22 02:27:06 +0200

    fg

--- Assets/EndSceneManager.cs
+++ Assets/EndSceneManager.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class EndSceneManager : MonoBehaviour
 {
+    public VideoPlayer vid;
+    public GameObject endGamePanel;
+    private void Start()
+    {
+
+        StartCoroutine(IsRunning());  
+        
+    }
     public void OnMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
@@ -12,5 +21,14 @@ public class EndSceneManager : MonoBehaviour
     public void OnQuitGame()
     {
         Application.Quit();
+    }
+
+    IEnumerator IsRunning()
+    {
+
+
+        yield return new WaitForSeconds(22);
+        Debug.Log("in");
+        endGamePanel.SetActive(true);
     }
 }
